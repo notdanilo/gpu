954f8a54218533e23c5509b3dc7e2359f8664992
Danilo Guanabara <danguafer@gmail.com>
2020-09-23 00:58:13 -0300

    Texture binding

--- src/code/programs/program.rs
+++ src/code/programs/program.rs
@@ -3,7 +3,7 @@
 //use crate::data::Sampler;
 
 use crate::prelude::*;
-use crate::{Context, GLContext};
+use crate::{Context, GLContext, Sampler, Texture2D};
 
 type ProgramResource = <glow::Context as HasContext>::Program;
 
@@ -33,15 +33,26 @@ impl Program {
 //            self::BindBufferBase(gl::SHADER_STORAGE_BUFFER, index, buffer.get_id());
 //        }
 //    }
-//    fn bind_texture(&mut self, texture:&dyn Texture, sampler:&Sampler, index:u32) {
-//        unsafe {
-//            gl::ActiveTexture(gl::TEXTURE0 + index);
-//            gl::BindTexture(texture.get_type(), texture.get_id());
-//            gl::BindSampler(index, sampler.get_id());
-//            gl::UseProgram(self.get_id());
-//            gl::Uniform1i(index as i32, index as i32);
-//        }
-//    }
+
+    /// Binds a `Texture2D` at `index` so it can be sampled with the specified `sampler`,
+    pub fn bind_texture_2d(&mut self, texture:&Texture2D, sampler:&Sampler, index:u32) {
+        unsafe {
+            self.gl.active_texture(glow::TEXTURE0 + index);
+            self.gl.bind_texture(glow::TEXTURE_2D, Some(texture.resource()));
+            self.gl.bind_sampler(index, Some(sampler.resource()));
+            self.gl.use_program(Some(self.resource()));
+            self.gl.uniform_1_i32(Some(&index), index as i32);
+        }
+    }
+
+    pub fn bind_vec2(&mut self, vec2:(f32, f32), index:u32) {
+        unsafe {
+            self.gl.uniform_2_f32(Some(&index), vec2.0, vec2.1);
+        }
+    }
+
+    //TODO: Unify bind_texture_2d with bind_image.
+
 //    fn bind_image(&mut self, texture: &dyn Texture, index: u32) {
 //        unsafe {
 //            gl::UseProgram(self.get_id());
